6a9b0ce8e06dd1e1635830964ecb442463e20802
bicxter <ilian.zmkh@gmail.com>
2023-11-11 17:41:41 +0000

    fixed again

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 const ending = document.querySelector('.ending')
-var bg = document.querySelector(".bg");
 
-// Get the button
-var btn = document.querySelector(".btn");
-
-// Pause and play the video, and change the button text
-btn.addEventListener('click', (e)=>{
-	console.log('hi')
-	if (bg.paused) {
-		bg.play();
-        btn.innerHTML = "<span class=\"material-symbols-outlined\">pause</span>";
-	} else {
-    	bg.pause();
-        btn.innerHTML = "<span class=\"material-symbols-outlined\">play_arrow</span>";
-  }
-})
 
 
 /*setInterval(()=>{
@@ -26,10 +11,10 @@ btn.addEventListener('click', (e)=>{
 }, 2500);*/
 
 const github = document.querySelector('.github')
-github.addEventListener("mouseenter", (e) => {document.querySelector(`.github`).innerHTML = '♱ /bicxter';})
+github.addEventListener("mouseenter", (e) => {document.querySelector(`.github`).innerHTML = '♱ bicxter';})
 github.addEventListener("mouseleave", (e) => {document.querySelector(`.github`).innerHTML = `♱ github`;});
 const instagram = document.querySelector('.instagram')
-instagram.addEventListener("mouseenter", (e) => {document.querySelector(`.instagram`).innerHTML = '♱ bicxter0';})
+instagram.addEventListener("mouseenter", (e) => {document.querySelector(`.instagram`).innerHTML = '♱ iliancool123';})
 instagram.addEventListener("mouseleave", (e) => {document.querySelector(`.instagram`).innerHTML = `♱ instagram`;});
 const tiktok = document.querySelector('.tiktok')
 tiktok.addEventListener("mouseenter", (e) => {document.querySelector(`.tiktok`).innerHTML = '♱ fortniebals';})
