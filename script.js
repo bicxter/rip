b0619ecad7cfbfba14f0ca6ea47c8a7015b397cb
ilian <ilian.zmkh@gmail.com>
2025-10-17 13:09:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const ending = document.querySelector('.ending')
 }, 2500);*/
 
 const github = document.querySelector('.github')
-github.addEventListener("mouseenter", (e) => {document.querySelector(`.github`).innerHTML = '♱ bicxter';})
-github.addEventListener("mouseleave", (e) => {document.querySelector(`.github`).innerHTML = `♱ github`;});
+github.addEventListener("mouseenter", (e) => {document.querySelector(`.github`).innerHTML = '/bicxter';})
+github.addEventListener("mouseleave", (e) => {document.querySelector(`.github`).innerHTML = `✚ github`;});
 const tiktok = document.querySelector('.tiktok')
-tiktok.addEventListener("mouseenter", (e) => {document.querySelector(`.tiktok`).innerHTML = '♱ fortniebals';})
-tiktok.addEventListener("mouseleave", (e) => {document.querySelector(`.tiktok`).innerHTML = `♱ tiktok`;});
+tiktok.addEventListener("mouseenter", (e) => {document.querySelector(`.tiktok`).innerHTML = '@fortniebals';})
+tiktok.addEventListener("mouseleave", (e) => {document.querySelector(`.tiktok`).innerHTML = `✚ tiktok`;});
 
